a4b8355ee43afc3b7cf547b04f9b798d35e29dde
jmarcelotse <jmarcelotse@hotmail.com>
2024-09-06 13:38:46 -0300

    neww

---
+++ 6 Provisioners/2 remote-exec/main.tf
@@ -0,0 +1,75 @@
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-2"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+
+resource "aws_instance" "vm_aula" {
+  ami           = "ami-0c55b159cbfafe1f0" # Esta AMI é do Ubuntu 22.04 em us-east-2
+  instance_type = "t2.micro"
+  key_name      = var.key_name
+
+  # Definindo um security group básico para permitir SSH e HTTP
+  vpc_security_group_ids = [aws_security_group.sg.id]
+
+  tags = {
+    Name = "maquina-aula"
+  }
+
+  # Definindo a conexão SSH
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"                       # Usuário padrão para Ubuntu
+    private_key = file(var.private_key_path)     # Carregando a chave privada
+    host        = aws_instance.vm_aula.public_ip # Usando o IP público da instância
+  }
+
+  # Provisionamento remoto via SSH
+  provisioner "remote-exec" {
+    inline = [
+      #"sudo apt update",
+      "sudo DEBIAN_FRONTEND=noninteractive apt update",
+      "sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y",
+      "sudo apt install -y curl",
+      "sudo apt install -y nginx",
+      "curl -fsSL https://get.docker.com | sudo sh"
+    ]
+  }
+}
+
+resource "aws_security_group" "sg" {
+  name        = "sg_maquina_aula"
+  description = "Permitir acesso SSH e HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
